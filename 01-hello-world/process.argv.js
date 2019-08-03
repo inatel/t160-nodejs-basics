d85df9d99cec17b8fef23eb59d58534972ef767a
Aluno do Inatel <aluno@local.inatel.br>
2019-08-03 16:17:41 -0300

    Update process.argv.js

--- 01-hello-world/process.argv.js
+++ 01-hello-world/process.argv.js
@@ -1,1 +1,12 @@
-console.log(process.argv);
+const values = process.argv.slice(2);
+
+function regularPrint(value) {
+    console.log('The value is: ' + value);
+}
+
+function fancyPrint(value) {
+    console.log(`---> ${ value } <---`);
+}
+
+const print = eval(process.env.PRINTER) || regularPrint;
+values.forEach(print);
