ffbbd918a26e5a0dca15c1a6108aace15c46af59
Edy Segura <edysegura@gmail.com>
2019-08-08 22:08:00 -0300

    Update identation on javascript-io

--- 02-file-system/javascript-io.js
+++ 02-file-system/javascript-io.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 
 function printFileContent(error, data) {
-   if(error) {
-       console.log('File not found!', error);
-       return;
-   }
-   console.log(data);
+  if (error) {
+    console.log('File not found!', error);
+    return;
+  }
+  console.log(data);
 }
 
 fs.readFile('./students.txt', 'utf8', printFileContent);
