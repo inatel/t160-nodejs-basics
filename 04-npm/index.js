ba57c75804934721bcf7b9785ac7ca23eae4bf46
Aluno do Inatel <aluno@local.inatel.br>
2019-08-10 17:28:01 -0300

    Add lodash dependency to npm example

--- 04-npm/index.js
+++ 04-npm/index.js
@@ -1,5 +1,6 @@
 const chalk = require('chalk');
 const string = require('useful-string');
+const times = require('lodash/times');
 
 console.log(chalk.blue.underline('Hi NPM modules!!'));
-console.log('id: ' + string.guid());
+times(4, () => console.log(string.guid()));
