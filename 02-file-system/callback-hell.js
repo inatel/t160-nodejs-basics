71ecbd6aad071465879ff0071b1755610dced9b8
Aluno do Inatel <aluno@local.inatel.br>
2019-08-10 10:52:57 -0300

    Complete callback hell

--- 02-file-system/callback-hell.js
+++ 02-file-system/callback-hell.js
@@ -5,6 +5,10 @@ const fileName = 'my-file.txt';
 fs.readFile(fileName, 'utf-8', (error, data) => {
   if (error) throw error;
   fs.writeFile(fileName, data + '\nCallback Hell', (error) => {
-    console.log("It's works");
+    if(error) throw error;
+    fs.writeFile('my-logfile.txt', new Date().toLocaleString(), (error) => {
+      if(error) throw error;
+      console.log('Process has been finished!');
+    })
   })
-})
+});
