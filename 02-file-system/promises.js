7146a43719c49a48e6ed8e5037164e646785f3fd
Aluno do Inatel <aluno@local.inatel.br>
2019-08-10 11:44:58 -0300

    Promises second example

--- 02-file-system/promises.js
+++ 02-file-system/promises.js
@@ -1,11 +1,12 @@
 
 function getWeatherForecast() {
   return new Promise((resolve, reject) => {
-    setTimeout(() => resolve('Sunshine'), 1000);
+    setTimeout(() => reject('Sorry dad, I could not get it.'), 1000);
   });
 }
 
 getWeatherForecast()
-  .then(weatherForecast => console.log(weatherForecast));
+  .then(weatherForecast => console.log(weatherForecast))
+  .catch(error => console.log(error))
 
 console.log('Coffee is ready!!! Came!');
