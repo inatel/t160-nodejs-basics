f3aa37c95bd2a24cdc5c1a5f3f4d32f6e92eab7a
Edy Segura <edysegura@gmail.com>
2019-08-13 22:21:55 -0300

    Add http-server example

---
+++ 05-http-server/server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const host = 'localhost';
+const port = 3000;
+
+function requestHandler(request, response) {
+  const { method, url } = request;
+  console.log(`${method}: ${url}`);
+  response.end('Hi there!');
+}
+
+http.createServer(requestHandler)
+  .listen(port, () => {
+    console.log(`Server up on http://${host}:${port}`);
+  });
