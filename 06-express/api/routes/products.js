8b88d04b0d3b29fe69c8cf04665bab0e55247298
Edy Segura <edysegura@gmail.com>
2019-08-14 22:26:15 -0300

    Update response messages

--- 06-express/api/routes/products.js
+++ 06-express/api/routes/products.js
@@ -2,21 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (request, response) => {
-  response.status(200).json({
-    message: 'Handling POST requests to /products'
+  response.status(201).json({
+    message: 'Product was created'
   });
 });
 
 router.get('/', (request, response) => {
   response.status(200).json({
-    message: 'Handling GET requests to /products'
+    message: 'Products were fetched'
   });
 });
 
 router.get('/:productId', (request, response) => {
   const id = request.params.productId;
   response.status(200).json({
-    message: 'You passed an ID',
+    message: `Product with ID = ${id} was fetched`,
     id
   });
 });
