e23ae688444bef17278ef3498257642fcbd2c1c6
Edy Segura <edysegura@gmail.com>
2019-08-14 21:49:00 -0300

    Add route to get by id

--- 06-express/api/routes/products.js
+++ 06-express/api/routes/products.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+router.post('/', (request, response) => {
+  response.status(200).json({
+    message: 'Handling POST requests to /products'
+  });
+});
+
 router.get('/', (request, response) => {
   response.status(200).json({
     message: 'Handling GET requests to /products'
   });
 });
 
-router.post('/', (request, response) => {
+router.get('/:productId', (request, response) => {
+  const id = request.params.productId;
   response.status(200).json({
-    message: 'Handling POST requests to /products'
+    message: 'You passed an ID',
+    id
   });
 });
 
