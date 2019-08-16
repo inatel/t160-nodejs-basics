1b284327c4a036375cd4b4a8a7e13c48a247e2ee
Edy Segura <edysegura@gmail.com>
2019-08-15 21:54:28 -0300

    Add morgan to log the requests

--- 06-express/app.js
+++ 06-express/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
+
+// Logging
+app.use(morgan('dev'));
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
