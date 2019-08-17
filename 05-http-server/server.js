483da29dacc45011aac39f5cb05f8a91a7784f18
Aluno do Inatel <aluno@local.inatel.br>
2019-08-17 10:11:45 -0300

    Solve HTML challenge

--- 05-http-server/server.js
+++ 05-http-server/server.js
@@ -1,11 +1,16 @@
 const http = require('http');
+const fs = require('fs');
+const { promisify } = require('util');
+
+const readFile = promisify(fs.readFile);
 
 const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 3000;
 
 function requestHandler(request, response) {
   console.log(request.method, request.url);
-  response.end('Hi there again!');
+  readFile('./index.html')
+    .then( data => response.end(data));
 }
 
 http.createServer(requestHandler)
